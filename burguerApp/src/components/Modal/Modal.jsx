5b6e4d6044e406b89c2a7609d018bf3e6bc0bba1
Redf0xD <ricardoprato36@gmail.com>
2022-07-10 08:59:07 -0400

    :fire: Eliminando boton para cerrar modal y sus estilos

--- burguerApp/src/components/Modal/Modal.jsx
+++ burguerApp/src/components/Modal/Modal.jsx
@@ -1,21 +1,11 @@
 import ReactDOM from 'react-dom';
 import styles from './Modal.module.scss';
-import { FaArrowLeft } from 'react-icons/fa';
 
-export const Modal = ({ children, setShowModal, fill = 'white' }) => {
-  const functionModal = () => {
-    setShowModal((prev) => !prev);
-  };
-
+export const Modal = ({ children }) => {
   const div = document.getElementById('modal');
   return ReactDOM.createPortal(
     <div className={styles.modal}>
-      <div className={styles.modal_container}>
-        <button onClick={functionModal} className={styles.button}>
-          <FaArrowLeft style={{ fill: 'black' }} />
-        </button>
-        {children}
-      </div>
+      <div className={styles.modal_container}>{children}</div>
     </div>,
     div
   );
