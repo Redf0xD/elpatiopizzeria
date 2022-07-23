2f1b3919e8049391541a84b45156eadb68643910
Redf0xD <ricardoprato36@gmail.com>
2022-07-23 09:17:15 -0400

    :sparkles: hook para medir el ancho de la pantalla

---
+++ burguerApp/src/hooks/useWidth.jsx
@@ -0,0 +1,21 @@
+import { useState, useSyncExternalStore } from 'react'
+const div = document.createElement('div')
+div.style.overflowY = 'scroll'
+document.body.append(div)
+const scrollWidth = div.offsetWidth - div.clientWidth
+
+export const useWidth = () => {
+  const [width, setWidth] = useState(false)
+  const subscribe = callback => {
+    window.addEventListener('resize', callback)
+    return () => {
+      window.removeEventListener('resize', callback)
+    }
+  }
+  const getSnapshot = () => {
+    const newWitdh = window.innerWidth - scrollWidth
+    width !== newWitdh && setWidth(newWitdh)
+  }
+  useSyncExternalStore(subscribe, getSnapshot)
+  return { width }
+}
