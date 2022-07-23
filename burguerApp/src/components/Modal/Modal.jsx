6b7d0dd0ea70c57c314dc0e8f842cf5bd5ac7bc5
Redf0xD <ricardoprato36@gmail.com>
2022-07-23 09:17:55 -0400

    :art: añadiendo setModal al div para cerrar sin necesidad de un boton

--- burguerApp/src/components/Modal/Modal.jsx
+++ burguerApp/src/components/Modal/Modal.jsx
@@ -1,12 +1,17 @@
-import ReactDOM from 'react-dom';
-import styles from './Modal.module.scss';
+import ReactDOM from 'react-dom'
+import styles from './Modal.module.scss'
 
-export const Modal = ({ children }) => {
-  const div = document.getElementById('modal');
+export const Modal = ({ children, setModal = (prop) => prop }) => {
+  const div = document.getElementById('modal')
+  const handleClick = () => {
+    setModal(false)
+  }
   return ReactDOM.createPortal(
     <div className={styles.modal}>
-      <div className={styles.modal_container}>{children}</div>
+      <div onClick={handleClick} className={styles.modal_container}>
+        {children}
+      </div>
     </div>,
     div
-  );
-};
+  )
+}
