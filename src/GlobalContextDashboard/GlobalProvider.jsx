86a120c298b34e6836b4b59dde0225f537c97552
Luciano Leyria <luciano.leyria96@gmail.com>
2022-11-11 16:20:39 -0300

    token solucionado

--- src/GlobalContextDashboard/GlobalProvider.jsx
+++ src/GlobalContextDashboard/GlobalProvider.jsx
@@ -1,7 +1,7 @@
-import React, { useReducer } from 'react'
-import { GlobalContext } from './GlobalContext'
-import { reducer } from './GlobalReducer'
-import { useNavigate } from 'react-router-dom'
+import React, { useReducer } from "react";
+import { GlobalContext } from "./GlobalContext";
+import { reducer } from "./GlobalReducer";
+import { useNavigate } from "react-router-dom";
 import {
   obtenerCategorias,
   agregarCategorias,
@@ -13,8 +13,8 @@ import {
   modificarProductos,
   agregarVariantes,
   borrarVariantes,
-  modificarVariantes
-} from '../Fetchs'
+  modificarVariantes,
+} from "../Fetchs";
 import {
   GET_CATEGORIES,
   DELETE_CATEGORIES,
@@ -26,147 +26,148 @@ import {
   ADD_PRODUCTS,
   ADD_VARIANTS,
   DELETE_VARIANTS,
-  MODIFY_VARIANTS
-} from './types'
+  MODIFY_VARIANTS,
+} from "./types";
 
 export const ContextProvider = ({ children }) => {
   // Estado inicial
   const initialState = {
     categories: [],
-    products: []
-  }
-  const [state, dispatch] = useReducer(reducer, initialState)
+    products: [],
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
   const navigate = useNavigate();
 
   ///funciones que modifican estados
   const setCategories = () => {
     obtenerCategorias()
-      .then(data => {
-        if(data.error) return navigate("/ingresar");
+      .then((data) => {
+        if (data.error) return navigate("/ingresar");
         dispatch({
           type: GET_CATEGORIES,
-          payload: data
-        })
+          payload: data,
+        });
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
-  const addCategories = categoria => {
+  const addCategories = (categoria) => {
     agregarCategorias(categoria)
-      .then(data => {
-        if(data.error) return navigate("/ingresar");
+      .then((data) => {
+        if (data.error) return navigate("/ingresar");
         dispatch({
           type: ADD_CATEGORIES,
-          payload: data
-        })
+          payload: data,
+        });
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
-  const deleteCategories = id => {
+  const deleteCategories = (id) => {
     borrarCategorias(id)
       .then((data) => {
-        if(data.error) return navigate("/ingresar");
+        if (data.error) return navigate("/ingresar");
         dispatch({
           type: DELETE_CATEGORIES,
-          payload: id
-        })
+          payload: id,
+        });
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   const modifyCategories = (id, categoriaModificada) => {
     modificarCategorias(id, categoriaModificada)
-      .then(data => {
-        if(data.error) return navigate("/ingresar");
+      .then((data) => {
+        if (data.error) return navigate("/ingresar");
         dispatch({
           type: MODIFY_CATEGORIES,
-          payload: data
-        })
+          payload: data,
+        });
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   const getProducts = () => {
     obtenerProductos()
-      .then(data => {
+      .then((data) => {
         dispatch({
           type: GET_PRODUCTS,
-          payload: data
-        })
+          payload: data,
+        });
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
-  const addProducts = producto => {
+  const addProducts = (producto) => {
     agregarProductos(producto)
-      .then(data => {
-        if(data.error) return navigate("/ingresar");
+      .then((data) => {
+        if (data.error) return navigate("/ingresar");
         dispatch({
           type: ADD_PRODUCTS,
-          payload: data
-        })
+          payload: data,
+        });
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
-  const deleteProducts = id => {
+  const deleteProducts = (id) => {
     borrarProductos(id)
       .then((data) => {
-        if(data.error) return navigate("/ingresar");
+        if (data.error) return navigate("/ingresar");
         dispatch({
           type: DELETE_PRODUCTS,
-          payload: id
-        })
+          payload: id,
+        });
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   const modifyProducts = (id, productoModificado) => {
     modificarProductos(id, productoModificado)
-      .then(data => {
-        if(data.error) return navigate("/ingresar");
+      .then((data) => {
+        if (data.error) return navigate("/ingresar");
         dispatch({
           type: MODIFY_PRODUCTS,
-          payload: data
-        })
+          payload: data,
+        });
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
-  const addVariants = variante => {
-    agregarVariantes(variante).then(data => {
-      if(data.error) return navigate("/ingresar");
+  const addVariants = (variante) => {
+    agregarVariantes(variante).then((data) => {
+      if (data.error) alert("Se produjo un error");
+      console.log("PEPEEEEE", data);
       dispatch({
         type: ADD_VARIANTS,
-        payload: data
-      })
-    })
-  }
+        payload: data,
+      });
+    });
+  };
 
   const deleteVariants = (id, productId) => {
     borrarVariantes(id)
       .then((data) => {
-        if(data.error) return navigate("/ingresar");
+        if (data.error) return navigate("/ingresar");
         dispatch({
           type: DELETE_VARIANTS,
-          payload: { id, productId }
-        })
+          payload: { id, productId },
+        });
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   const modifyVariants = (id, varianteModificada, productId) => {
     modificarVariantes(id, varianteModificada, productId)
-      .then(data => {
-        if(data.error) return navigate("/ingresar");
+      .then((data) => {
+        if (data.error) return navigate("/ingresar");
         dispatch({
           type: MODIFY_VARIANTS,
-          payload: { data, id, productId }
-        })
+          payload: { data, id, productId },
+        });
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div>
@@ -185,11 +186,11 @@ export const ContextProvider = ({ children }) => {
           modifyProducts,
           addVariants,
           deleteVariants,
-          modifyVariants
+          modifyVariants,
         }}
       >
         {children}
       </GlobalContext.Provider>
     </div>
-  )
-}
+  );
+};
