c3793b9c58aef0aebf96559c9fa5c19a537cdef6
Luciano Leyria <luciano.leyria96@gmail.com>
2022-11-11 16:55:59 -0300

    solucionado bug en delivery

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -193,7 +193,7 @@ export const Cart = () => {
                       className={styles.input}
                     />
                   </label>
-                  <label>
+                  {/* <label>
                     Provincia
                     <input
                       onChange={handleChange}
@@ -202,7 +202,7 @@ export const Cart = () => {
                       required
                       className={styles.input}
                     />
-                  </label>
+                  </label> */}
                 </div>
               )}
               <p>
@@ -246,7 +246,7 @@ export const Cart = () => {
                   </label>
                 </>
               )}
-              <label htmlFor="Lo antes posible" className={styles.label}>
+              <label htmlFor="lo antes posible" className={styles.label}>
                 Lo antes posible
                 <input
                   onChange={handleChange}
